0e3aa4dac10e6de7841b62bb0d9b9d1c30dda02d
yauhen <yauhentim@gmail.com>
2024-09-03 23:54:44 +0200

    Fix JSON response handling and improve error reporting

--- json.go
+++ json.go
@@ -10,10 +10,12 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	dat, err := json.Marshal(payload)
 
 	if err != nil {
-		log.Printf("Failed to marshal JSON response: %v\n", payload)
-		w.WriteHeader(500)
+		log.Printf("Failed to marshal JSON response: %v\n", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
 	}
-	w.Header().Add("Content-Type", "application/json")
+
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
 	w.Write(dat)
 }
